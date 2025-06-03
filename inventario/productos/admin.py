1024288b0cde1a9623c57c0481dd5ab36c4bd2b8
ARGENIS RODRIGUEZ PRIETO <135769051+Argenis2023@users.noreply.github.com>
2025-06-03 18:16:57 -0400

    Añadir categoría y precio actual en la lista de productos; implementar botón para ver historial de precios.

--- inventario/productos/admin.py
+++ inventario/productos/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from .models import Producto
-from .models import Movimiento, Proveedor
+from .models import Movimiento, Proveedor, Categoria
 
 admin.site.register(Producto)
 admin.site.register(Movimiento)
 admin.site.register(Proveedor)
+admin.site.register(Categoria)
